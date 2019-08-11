117303582352f25582598a6c96faca65c9d7b74d
Geraldo <geraldo.figueidosj@gmail.com>
2019-08-11 14:33:34 -0300

    012 intialized

--- 002/main.go
+++ 002/main.go
@@ -3,6 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int32{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
+	// numbers := []int{3, 2, 1}
 	fmt.Println(numbers)
+	fmt.Println(productAndDivision(&numbers))
+}
+
+func productAndDivision(numbers *[]int) *[]int {
+	newArray := make([]int, len(*numbers))
+	newArray[0] = 1
+
+	for i := 1; i < len(*numbers); i++ {
+		newArray[0] *= (*numbers)[i]
+	}
+
+	for i := 1; i < len(*numbers); i++ {
+		newArray[i] = (newArray[i-1] / (*numbers)[i]) * (*numbers)[i-1]
+	}
+
+	return &newArray
 }
