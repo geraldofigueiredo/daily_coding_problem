1c0fd754cfbdb1736d21c507e39ecbd8e56c30ee
Geraldo <geraldo.figueidosj@gmail.com>
2019-08-11 13:49:00 -0300

    001 finalized

--- 001/main.go
+++ 001/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int32{1, 4, 5, 6, 9, 13, 36, 78, 45}
-	searchAddup(&numbers, 20)
+	numbers := []int32{10, 15, 3, 7}
+	fmt.Println(searchAddup(&numbers, 17))
 }
 
-func searchAddup(numbers *[]int32, k int32) {
+func searchAddup(numbers *[]int32, k int32) (bool, int32, int32) {
 	fmt.Println(*numbers)
+	for i := 0; i < len(*numbers); i++ {
+		for j := i + 1; j < len(*numbers); j++ {
+			if (*numbers)[i]+(*numbers)[j] == k {
+				return true, (*numbers)[i], (*numbers)[j]
+			}
+		}
+	}
+	return false, 0, 0
 }
